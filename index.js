77a3dd03cdb8ca9ce59477d413fb7fc10df21b54
will-lp <will_lp@ymail.com>
2019-05-06 19:39:14 -0300

    Adicionado CORS

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const port = 3000;
 const person = require('./person.js');
 
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 app.get('/person/list', (req, res) => {
   return res.send(person.list());
 });
