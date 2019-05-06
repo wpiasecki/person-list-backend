f6339e31f55a2ec4a0f31bf84d299401c5389a13
will-lp <will_lp@ymail.com>
2019-05-06 17:14:07 -0300

    Resource de pessoas refatorado para arquivo separado.

--- index.js
+++ index.js
@@ -2,30 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.listen(port);
-
-
-const listPeople = () => {
-  const names = [ "Adam", "Abe", "Maria", "Rose", "Mario", "Luigi" ];
-  const surnames = [ "Lincoln", "Franklin", "Jackson", "Miyazaki", "M'bebe" ];
-  const people = [];
-  const rnd = length => Math.floor(Math.random() * length);
-
-  for (let i = 0; i < 10; i++) {
-    people.push({
-      id: i,
-      name: names[rnd(names.length)] + " " + surnames[rnd(surnames.length)],
-      disclosableInfo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit," + 
-        "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad mini" +
-        "m veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo c"+
-        "onsequat."
-    });
-  }
-  
-  return people;
-};
+const person = require('./person.js');
 
 
 app.get('/person/list', (req, res) => {
-  return res.send(listPeople());
+  return res.send(person.list());
 });
+
+
+app.listen(port);
+
